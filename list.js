21c19632738b67ff724cd6c438956b3e25b4d20b
RANJITHAKK <102507813+RANJITHAKK@users.noreply.github.com>
2022-04-25 10:10:18 +0530

    last list

---
+++ list.js
@@ -0,0 +1,122 @@
+// promise counter function
+let completed = 0,
+  incomplete = 0;
+let count = 0;
+function counter(elem) {
+  let promise = new Promise((res, rej) => {
+    if (elem.checked) {
+      count++;
+      completed++;
+    } else {
+      count--;
+      completed--;
+    }
+    res(count);
+  });
+
+  promise.then((value) => {
+    if (value == 5) {
+      $(".popup").css("visibility", "visible");
+      
+    }
+    else if(value >= 6) {
+      var s = `You have completed ${value} tasks`;
+      $(".popup1").css("visibility", "visible");
+      document.getElementById("popy").innerHTML =  s;
+    }
+   
+  });
+
+  // incomplete = 200 - completed;
+
+  // $(".stat").css("visibility", "visible");
+
+  // document.getElementById("status").innerHTML = `<td>${completed}</td>
+  //                           <td>${incomplete}</td>`;
+}
+
+// --------
+
+// popup message
+
+$("#popbtn").on("click", () => {
+  $(".popup").css("visibility", "hidden");
+});
+$("#popbtn1").on("click", () => {
+  $(".popup1").css("visibility", "hidden");
+});
+
+// ------
+
+$(document).ready(() => {
+  // ajax get and populate list on todo button
+
+  $(".todo").on("click", () => {
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/todos",
+      success: (data) => {
+        var list = document.getElementById("list");
+        var row = "";
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].completed == true) {
+            row = `<input class="cboxComp" type="checkbox" checked disabled > 
+            &emsp;<span class="datalistComp">
+              <s style="color: rgb(214, 106, 106)" >
+                <small style="color: #dcdcde">${data[i].title}
+                </small>
+              </s>
+            </span><br><hr>`;
+            completed++;
+          } else {
+            row = `<input class="cbox" type="checkbox" onchange="counter(this)" > 
+            &emsp;<span class="datalist">
+                <span style="color: white">${data[i].title}
+                </span>
+            </span><br><hr>`;
+          }
+
+          list.innerHTML += row;
+        }
+      },
+    });
+    $("#list").css("visibility", "visible");
+  });
+
+  //-------
+
+  // logout button
+
+  $(".logout").on("click", () => {
+    window.location.href = "./index.html";
+  });
+
+  // -----
+
+  // strike throught function
+
+  function strike() {
+    var chbox = Array.from(document.querySelectorAll(".cbox"));
+    var dataTitle = Array.from(document.querySelectorAll(".datalist"));
+
+    for (var i = 0; i < chbox.length; i++) {
+      if (chbox[i].checked) {
+        dataTitle[i].innerHTML = `
+                <span class = "strike"><small style = "color: #dcdcde" >${dataTitle[i].textContent}
+                </small></span>
+                `;
+      } else {
+        dataTitle[
+          i
+        ].innerHTML = `<span style="color: white">${dataTitle[i].textContent}</span>`;
+      }
+    }
+  }
+
+  // -----
+
+  // strike through event call
+
+  $("#list").on("click", strike);
+});
+
+// style = "color: rgb(161, 228, 161)";
---
+++ login-script.js
@@ -0,0 +1,36 @@
+  
+    $(".btn").click(() => {
+      validate(redirect);
+    });
+  
+    function validate(callback) {
+      var check = true;
+      let uname = $(".uname");
+      let pass = $(".pass");
+  
+      if (uname[0].value != "admin") {
+        uname.css("border", "solid red 2px");
+        $(".uname+.fas").css("visibility", "visible");
+        check = false;
+      } else {
+        uname.css("border", "none");
+        $(".uname+.fas").css("visibility", "hidden");
+      }
+  
+      if (pass[0].value != 12345) {
+        pass.css("border", "solid red 2px");
+        $(".pass+.fas").css("visibility", "visible");
+        check = false;
+      } else {
+        pass.css("border", "none");
+        $(".pass+.fas").css("visibility", "hidden");
+      }
+  
+      callback(check);
+    }
+  
+    function redirect(condn) {
+      if (condn == true) {
+        window.location.href = "./todolist.html";
+      }
+    }
